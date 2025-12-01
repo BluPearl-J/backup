9986be49d9bf3581e805728972ea1d8691ef5d02
BluPearl-J <ogheneokeno@gmail.com>
2025-12-01 10:14:23 +0100

    Add files via upload

---
+++ group_two_app/src/main/java/org/weddingPlanner/data/models/Checklist.java
@@ -0,0 +1,15 @@
+package org.weddingPlanner.data.models;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+
+@Data
+public class Checklist {
+    @Id
+    private String id;
+    private String name;
+    private List<Task> tasks;
+}
